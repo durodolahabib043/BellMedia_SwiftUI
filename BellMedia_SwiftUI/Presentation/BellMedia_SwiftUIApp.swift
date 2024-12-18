f842bdba5711a5595d833d4d972518fcee65841c
durodolahabib043 <durodolahabib043@gmail.com>
2024-12-18 10:53:30 -0500

    first commit

---
+++ BellMedia_SwiftUI/Presentation/BellMedia_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellMedia_SwiftUIApp.swift
+//  BellMedia_SwiftUI
+//
+//  Created by Habib Durodola on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BellMedia_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BellMedia_SwiftUI/Presentation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BellMedia_SwiftUI
+//
+//  Created by Habib Durodola on 12/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        NavigationStack {
+            VStack{
+                Text("Hello World")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
