6c8b7eebfc5424af7f70e48c29c3c1e7b4876789
durodolahabib043 <durodolahabib043@gmail.com>
2025-01-24 13:33:38 -0500

    refactor to add list, expandable works fine

---
+++ BellMedia_SwiftUI/Presentation/ListCell.swift
@@ -0,0 +1,20 @@
+//
+//  ListCell.swift
+//  BellMedia_SwiftUI
+//
+//  Created by Habib Durodola on 1/24/25.
+//
+
+import SwiftUI
+
+struct ListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ListCell()
+    }
+}
